ee9840303faa3b2761f17fbb8db7c67ecd350f53
Junji <junji9193@outlook.jp>
2023-04-03 23:12:45 +0900

    202304032312

--- Assets/Scripts/Tool/ScreenShotCapturer.cs
+++ Assets/Scripts/Tool/ScreenShotCapturer.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 using System.IO;
 using System;
 using System.Text;
+using UnityEngine.InputSystem;
 
 public class ScreenShotCapturer : MonoBehaviour
 {
-    //private void Update()
-    //{
-    //    if (Input.GetKeyDown(KeyCode.Space))
-    //    {
-    //        DateTime dateTime = DateTime.Now;
-    //        CaptureScreenShot(@"C:\Users\junji\log\" + dateTime.Year.ToString() + "_" + dateTime.Month.ToString() + "_" + dateTime.Day.ToString() + "_" + dateTime.Hour.ToString() + "_" + dateTime.Minute.ToString() + "_" + dateTime.Second.ToString() + ".png");
-    //    }
-    //}
+    private void Update()
+    {
+        if (Keyboard.current.spaceKey.isPressed)
+        {
+            DateTime dateTime = DateTime.Now;
+            CaptureScreenShot(Application.dataPath + @"\ScreenShots\"
+                            + dateTime.Year.ToString() + "_" 
+                            + dateTime.Month.ToString() + "_" 
+                            + dateTime.Day.ToString() + "_" 
+                            + dateTime.Hour.ToString() + "_" 
+                            + dateTime.Minute.ToString() + "_" 
+                            + dateTime.Second.ToString() + ".png");
+        }
+    }
 
-    //private void CaptureScreenShot(string filePath)
-    //{
-    //    ScreenCapture.CaptureScreenshot(filePath);
-    //}
+    private void CaptureScreenShot(string filePath)
+    {
+        ScreenCapture.CaptureScreenshot(filePath);
+    }
 }
